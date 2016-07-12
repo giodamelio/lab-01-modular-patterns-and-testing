15bf39830b15eeb93daf335e9de4476ae213597a
Gio d'Amelio <giodamelio@gmail.com>
2016-07-11 18:31:59 -0700

    Create package.json. Add basic cowsay function

---
+++ lab-giodamelio/src/cowsay.js
@@ -0,0 +1,18 @@
+module.exports = function(input) {
+  const topOfBubble = '╔' + '═'.repeat(input.length + 2) + '╗\n';
+  const transformedInput = '║ ' + input + ' ║\n';
+  const bottomOfBubble = '╚═╤' + '═'.repeat(input.length) + '╝';
+  const body = `
+  │       ,-^-.
+  │       !oYo!
+  ╰───── /./=\\.\\______
+            ##        )\\/\\
+             ||-----w||
+             ||      ||
+             Vader Cow!
+  `;
+
+  return topOfBubble + transformedInput + bottomOfBubble + body;
+};
+
+console.log(module.exports('Hello World!'));
