8b1ba903d1084d7364d9098f7937ada642a79871
Gio d'Amelio <giodamelio@gmail.com>
2016-07-13 09:51:14 -0700

    Run eslint with gulp

--- lab-giodamelio/gulpfile.js
+++ lab-giodamelio/gulpfile.js
@@ -1,6 +1,21 @@
 const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+
+const SRC_FILES = 'src/**/*.js';
+const TEST_FILES = 'test/**/*.js';
 
 gulp.task('lint:src', function() {
+  return gulp.src(SRC_FILES)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
-gulp.task('default', ['lint:src']);
+gulp.task('lint:test', function() {
+  return gulp.src(TEST_FILES)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('default', ['lint:src', 'lint:test']);
